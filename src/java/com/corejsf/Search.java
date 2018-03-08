e68fc40f362fb664ea0d283c8384df430689f2de
Justin Wheeler <btxbaky@gmail.com>
2018-03-08 20:49:33 +0900

    Small comment change

--- src/java/com/corejsf/Search.java
+++ src/java/com/corejsf/Search.java
@@ -1,11 +1,10 @@
 package com.corejsf;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.io.FileNotFoundException;
+import java.io.Serializable;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,53 +13,69 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
 /**
- *
- * @author Justin Wheeler.
+ * @author Justin Wheeler
  */
-
 @ManagedBean(name = "search", eager = true)
 @SessionScoped
-public class Search {
-  private String selection;
-  private String user_in;
-  static Connection conn;
-  private String sta = "SELECT * from REVIEWS";
-  
-  public List<Reviews> getSearch() throws SQLException{
-    String connectionURL = "";  
-    conn = DriverManager.getConnection(connectionURL);
-    List<Reviews> searches = new ArrayList<Reviews>();
-    if (selection != null && user_in != null){
-        //Its important to add '' when searching for strings
-        if (selection.equalsIgnoreCase("review_id") || selection.equalsIgnoreCase("review_score")){
-            sta = "SELECT * from REVIEWS where "+selection+" = "+user_in;                        
-        }else
-            sta = "SELECT * from REVIEWS where "+selection+" = '"+user_in+"'";
-    }
-    
-    try {
-        PreparedStatement pst = conn.prepareStatement(sta);
-        pst.execute();
-        ResultSet rs = pst.getResultSet();
-        
-        while(rs.next()){
-            Reviews search = new Reviews();
-            search.setId(rs.getInt(1));
-            search.setGameName(rs.getString(2));
-            search.setConsole(rs.getString(3));
-            search.setYear(rs.getString(4));
-            search.setReviewer(rs.getString(5));
-            search.setReviewScore(rs.getInt(6));
-            
-            //Add the parsed info to the ArrayList
-            searches.add(search);
-        }
-    }catch(SQLException e){
-        e.printStackTrace();
-    }  
-    return searches;
-  }
+public class Search implements Serializable {
+    private ConnectDB connection;
+    private String selection;
+    private String user_in;
+    private String query;
 
+    //Using constants to avoid misuse
+    private static final String SELECTALL = "*";
+    private static final String TABLENAME = "REVIEWS";
+
+    public List<Reviews> getSearch() throws SQLException, FileNotFoundException {
+        connection = new ConnectDB();
+
+        List<Reviews> searches = new ArrayList<>();
+        if (selection != null && user_in != null) {
+
+            //Integers require a different input when searching so I seperated them
+            if (selection.equalsIgnoreCase("review_id") || selection.equalsIgnoreCase("review_score")) {
+                
+                //Without parsing for an int this statement its possible to use SQL injection here
+                try {
+                    Integer.parseInt(user_in);
+                } catch (NumberFormatException ex) {
+                    System.out.println("User attempted to enter a value other than an integer.");
+                } finally {
+                    query = String.format("SELECT %s FROM %s WHERE %s = %s", SELECTALL, TABLENAME, selection, user_in);
+                }
+            } else {
+                //This queries string values hence the '' around %s
+                query = String.format("SELECT %s FROM %s WHERE %s LIKE '%s' ", SELECTALL, TABLENAME, selection, user_in);
+            }
+        }
+
+        try {
+            PreparedStatement pst = connection.conn.prepareStatement(query);
+            pst.execute();
+            ResultSet rs = pst.getResultSet();
+
+            while (rs.next()) {
+                Reviews search = new Reviews();
+                search.setId(rs.getInt(1));
+                search.setGameName(rs.getString(2));
+                search.setConsole(rs.getString(3));
+                search.setYear(rs.getString(4));
+                search.setReviewer(rs.getString(5));
+                search.setReviewScore(rs.getInt(6));
+
+                //Add the parsed info to the ArrayList
+                searches.add(search);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return searches;
+    }
+
+    /*
+    * Getters and Setters
+    */
     public void setSelection(String selection) {
         this.selection = selection;
     }
